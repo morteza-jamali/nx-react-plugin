056fa887035d4d87c5d11184865df73c77b5a648
morteza-jamali <mortezajamali4241@gmail.com>
2021-02-13 12:57:54 +0330

    custom webpack config feature created

--- polyfills/webpack.js
+++ polyfills/webpack.js
@@ -1,14 +1,32 @@
-/*
-  - Solution for `nx react app gets 'global is not defined`
-  - https://github.com/nrwl/nx/issues/3673
-*/
-const nrwlConfig = require('@nrwl/react/plugins/webpack.js');
+const { resolve } = require("path");
+const { readdirSync, existsSync } = require("fs");
+const nrwlConfig = require("@nrwl/react/plugins/webpack.js");
 
 module.exports = (config, context) => {
+  const appsPath = resolve(context.buildOptions.root, "apps");
+
+  readdirSync(appsPath, { withFileTypes: true })
+    .filter((direct) => direct.isDirectory())
+    .forEach((dir) => {
+      const webpackConfigPath = resolve(
+        appsPath,
+        dir.name,
+        "webpack.config.js"
+      );
+
+      if (
+        existsSync(webpackConfigPath) &&
+        context.buildOptions.sourceRoot.includes(`apps/${dir.name}`)
+      ) {
+        const customConfig = require(webpackConfigPath);
+        config = customConfig(config);
+      }
+    });
+
   nrwlConfig(config);
 
   return {
     ...config,
-    node: { global: true, fs: 'empty' },
+    node: { global: true, fs: "empty" },
   };
 };
